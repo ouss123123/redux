20405d442652f593e1f145834a86610e79459581
ouss123123 <oussamafrere1@gmail.com>
2025-01-16 17:29:49 +0100

    ttttttttttttt

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { increment, decrement } from './features/counter/counterSlice';
+import Counter from './features/counter/Counter';
 import './App.css';
 
 const App = () => {
-    const count = useSelector((state) => state.counter.count);
-    const dispatch = useDispatch();
-
     return (
         <div className="App">
-            <h1>Counter: {count}</h1>
-            <button onClick={() => dispatch(increment())}>Increment</button>
-            <button onClick={() => dispatch(decrement())}>Decrement</button>
+            <Counter />
         </div>
     );
 };
